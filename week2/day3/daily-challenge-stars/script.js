49824a3eb435e66765f2771faaafa0a4fd8bdac2
NaamaCahen <cahenbb@gmail.com>
2022-10-20 21:30:13 +0300

    daily challenge stars

---
+++ week2/day3/daily-challenge-stars/script.js
@@ -0,0 +1,16 @@
+let star = '';
+for (let i = 1; i < 7; i++) {
+    star += ' * ';
+    console.log(star);
+}
+
+console.log('  ');
+
+
+for (let i = 0; i <=6; i++) {
+   let star2 = '';
+    for (let j = 0; j <i; j++) {
+        star2+=' * ';
+    }
+    console.log(star2);
+}
