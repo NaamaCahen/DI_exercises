6ca03250b4385303a44232ee2decbcfc4e031a19
NaamaCahen <cahenbb@gmail.com>
2022-10-21 13:47:03 +0300

    exercises 3 4

--- week2/day4/exercise-xp/script.js
+++ week2/day4/exercise-xp/script.js
@@ -30,18 +30,79 @@ function calculateTip() {
     // If the bill is between $50 and $200, tip 15%.
     // If the bill is more than $200, tip 10%.
     let tip;
-    if(bill<50)
-    {
-        tip=bill*0.2;
+    if (bill < 50) {
+        tip = bill * 0.2;
     }
-    else if(bill>=50 && bill<200){
-        tip=bill*0.15;
-    }else if (bill>=200){
-        tip=bill*0.1;
+    else if (bill >= 50 && bill < 200) {
+        tip = bill * 0.15;
+    } else if (bill >= 200) {
+        tip = bill * 0.1;
     }
     //Console.log the tip amount and the final bill (bill + tip).
-    console.log(`the tip amount is ${tip}$ and the final bill is ${(Number(bill))+tip}`);
+    console.log(`the tip amount is ${tip}$ and the final bill is ${(Number(bill)) + tip}`);
 
 }
 //Call the calculateTip() function.
 calculateTip();
+
+
+
+//exercise3
+//Create a function call isDivisible() that takes no parameter.
+function isDivisible(divisor) {
+    let sum = 0;
+    // In the function, loop through numbers 0 to 500.
+    for (let i = 0; i <= 500; i++)
+        // Console.log all the numbers divisible by 23.
+        if (i % divisor === 0) {
+            console.log(i);
+            sum += i;
+        }
+    // At the end, console.log the sum of all numbers that are divisible by 23.
+    console.log(sum);
+}
+//Bonus: Add a parameter divisor to the function.
+
+
+
+//exercise4
+//Add the stock and prices objects to your js file.
+const stock = {
+    "banana": 6,
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry": 1
+}
+
+const prices = {
+    "banana": 4,
+    "apple": 2,
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry": 10
+}
+// Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
+let shoppingList = ['banana', 'orange', 'apple'];
+//Create a function called myBill() that takes no parameters.
+//     The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+// The item must be in stock. 
+// If the item is in stock find out the price in the prices object.
+function myBill() {
+    let sum=0;
+    for (fruit of shoppingList) {
+        if(stock[fruit]>0){
+            sum+=prices[fruit];
+            stock[fruit]--;//Bonus: If the item is in stock, decrease the item’s stock by 1
+
+        }
+    }
+    console.log('you have to pay:'+sum);
+}
+// Call the myBill() function.
+myBill();
+
+
+
+
+
