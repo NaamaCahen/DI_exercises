e23a63fc5e2f7b402ed8fb6aefe17bcb82fc78f0
NaamaCahen <cahenbb@gmail.com>
2022-10-23 09:25:48 +0300

    modified

--- week2/day4/daily-challenge/script.js
+++ week2/day4/daily-challenge/script.js
@@ -1,14 +1,16 @@
 const promptInput = prompt("Enter comma separated words");
 const words = promptInput.split(",");
 //find the longest word
-let max=0;
-for(let i=0; i<words.length; i++){
-    if(words[i].length>max) max=words[i].length;
+let max = 0;
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > max) {
+        max = words[i].length;
+    }
 
 }
-console.log('*'.repeat(max+4));
-for(word of words){
-    console.log(`* ${word+' '.repeat(max-word.length)} *`);
+console.log('*'.repeat(max + 4));
+for (word of words) {
+    console.log(`* ${word + ' '.repeat(max - word.length)} *`);
 }
-console.log('*'.repeat(max+4));
+console.log('*'.repeat(max + 4));
 
