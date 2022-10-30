224e837c2390d6f94c840c1828f8a8acce97952c
NaamaCahen <cahenbb@gmail.com>
2022-10-30 13:37:36 +0200

    exercise2

---
+++ week4/exercises-xp/ex2/script.js
@@ -0,0 +1,15 @@
+// 🌟 Exercise 2 : Ternary Operator
+
+// Using the code below:
+
+// function winBattle(){
+//     return true;
+// }
+// Transform the winBattle() function to an arrow function.
+// Create a variable called experiencePoints.
+// Assign to this variable, a ternary operator. If winBattle() is true, the experiencePoints variable should be equal to 10, else the variable should be equal to 1.
+// Console.log the experiencePoints variable.
+const winBattle = () => true;
+let experiencePoints = 0;
+experiencePoints = winBattle() ? experiencePoints = 10 : experiencePoints = 1;
+console.log(experiencePoints);
