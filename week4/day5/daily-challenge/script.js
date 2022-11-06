10a5c206aecaca3c8c7d1da7fd67ee60eacdd4d7
NaamaCahen <cahenbb@gmail.com>
2022-11-06 09:50:45 +0200

    quicker way

--- week4/day5/daily-challenge/script.js
+++ week4/day5/daily-challenge/script.js
@@ -3,18 +3,18 @@ function isAnagram(string1,string2){
     const removeBlanks2=string2.replace(/\s/g,"");
     const arrLetters1=[...removeBlanks1];
     const arrLetters2=[...removeBlanks2];
-    arrLetters1.sort();
-    arrLetters2.sort();
-    flag=true;
-    if(arrLetters1.length===arrLetters2.length){
-        arrLetters1.forEach((element,index)=>{
-            if(element!==arrLetters2[index]){
-                flag=false;
-            }
-        })
-    }else{
-        flag=false;
-    }
-return flag;
+    return arrLetters1.sort().toString()===arrLetters2.sort().toString();
+    
+//     flag=true;
+//     if(arrLetters1.length===arrLetters2.length){
+//         arrLetters1.forEach((element,index)=>{
+//             if(element!==arrLetters2[index]){
+//                 flag=false;
+//             }
+//         })
+//     }else{
+//         flag=false;
+//     }
+// return flag;
 }
-console.log(isAnagram("aa bb","bb a a")); 
+console.log(isAnagram("aa bb","bb  a")); 
