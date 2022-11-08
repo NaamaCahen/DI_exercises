9a5d1442be52568fb78bd45f95323331377ab7c3
NaamaCahen <cahenbb@gmail.com>
2022-11-08 10:22:18 +0200

    change to random

--- week5/day2/daily-challenge/script.js
+++ week5/day2/daily-challenge/script.js
@@ -2,14 +2,17 @@ document.querySelector("form>button").addEventListener("click",(event)=>{
     const inputSearch=document.getElementById("search").value;
     event.preventDefault();
     const xhr=new XMLHttpRequest();
-    xhr.open("GET","https://api.giphy.com/v1/gifs/search?rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=1&q="+inputSearch)
+    xhr.open("GET","https://api.giphy.com/v1/gifs/random?rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&tag="+inputSearch)
     xhr.send();
     xhr.responseType="json";
     xhr.onload=()=>{
-        console.log(xhr.response);
+        if(xhr.status!==200){
+            console.error("ERROR");
+        }else{
+            console.log(xhr.response);
         const div=document.createElement("div");
         const image=document.createElement("img");
-        image.src=xhr.response.data[0].images.original.url;
+        image.src=xhr.response.data.images.original.url;
         const btn=document.createElement("button");
         btn.textContent="delete"
         div.append(image,btn)
@@ -17,6 +20,8 @@ document.querySelector("form>button").addEventListener("click",(event)=>{
         btn.addEventListener("click",(event)=>{
             event.preventDefault();
             div.remove()});
+        }
+        
     }
 })
 document.getElementById("deleteAll").addEventListener("click",(event)=>{
