622eca005365150a814ed959c5d2182b206d481e
NaamaCahen <cahenbb@gmail.com>
2022-11-13 22:40:31 +0200

    daily challenge modified

--- week6/day1/daily-challenge.sql
+++ week6/day1/daily-challenge.sql
@@ -2,3 +2,7 @@ SELECT COUNT(actor_id)
 FROM actors;
 INSERT INTO actors (first_name,last_name)
 VALUES('nono','taviovitz');
+-- the outcome:
+-- ERROR:  null value in column "age" of relation "actors" violates not-null constraint
+-- DETAIL:  Failing row contains (7, nono, taviovitz, null, null).
+-- SQL state: 23502
