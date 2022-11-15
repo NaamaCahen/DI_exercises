b46840c10e154f05f1f19a964b578e75c9a0839a
NaamaCahen <cahenbb@gmail.com>
2022-11-15 17:37:48 +0200

    xp exercise2

--- week6/day2/xp2.sql
+++ week6/day2/xp2.sql
@@ -13,18 +13,18 @@ SELECT phone FROM address WHERE district='Texas';
 -- Write a query to retrieve all movie details where the movie id is either 15 or 150.
 SELECT * FROM film WHERE film_id=15 OR film_id=150;
 -- Write a query which should check if your favorite movie exists in the database. Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
-SELECT film_id,title,description,release_year,rental_rate FROM film WHERE title='';
+SELECT film_id,title,description,release_year,rental_rate FROM film WHERE title='horsnormes';
 -- No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
-
+SELECT film_id,title,description,release_year,rental_rate FROM film WHERE  title ILIKE 'ho%';
 -- Write a query which will find the 10 cheapest movies.
 SELECT * FROM film ORDER BY rental_rate LIMIT 10;
 -- Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
 -- Bonus: Try to not use LIMIT.
-SELECT * FROM film ORDER BY rental_rate LIMIT 10;
+SELECT * FROM film ORDER BY rental_rate OFFSET 10 LIMIT 10;
 -- Write a query which will join the data in the customer table and the payment table. You want to get the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
-
+SELECT last_name , amount , payment_date FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id ORDER BY payment.customer_id;
 -- You need to check your inventory. Write a query to get all the movies which are not in inventory.
-
+SELECT title,inventory_id FROM film INNER JOIN inventory ON film.film_id=inventory.film_id WHERE film.film_id != inventory.film_id ;
 -- Write a query to find which city is in which country.
-
+SELECT city,country FROM city JOIN country ON city.country_id=country.country_id;
 -- Bonus You want to be able to see how your sellers have been doing? Write a query to get the customer’s id, names (first and last), the amount and the date of payment ordered by the id of the staff member who sold them the dvd.
