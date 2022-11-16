4c418b581ab79c39690ec965b69aece10532bbd9
NaamaCahen <cahenbb@gmail.com>
2022-11-16 19:03:50 +0200

    xp

--- week6/day4/xp/xp1.sql
+++ week6/day4/xp/xp1.sql
@@ -30,3 +30,22 @@ SELECT (first_name , last_name ) AS full_name , (CHAR_LENGTH(first_name) + CHAR_
 SELECT * FROM employees WHERE first_name ILIKE '%[0-9]%';
 -- Write a query to select the first ten records from a table.
 SELECT *FROM employees LIMIT 10;
+-- 🌟 Restricting And Sorting
+-- Write a query to display the first_name, last_name and salary of all employees whose salary is between $10,000 and $15,000.
+SELECT first_name, last_name,salary FROM employees WHERE salary BETWEEN 10000 AND 15000;
+-- Write a query to display the first_name, last_name and hire date of all employees who were hired in 1987.
+SELECT first_name, last_name,hire_date FROM employees WHERE EXTRACT(YEAR FROM hire_date)=1987;
+-- Write a query to get the all employees whose first_name has both the letters ‘c’ and ‘e’.
+SELECT * FROM employees WHERE first_name ILIKE '%c%' AND first_name ILIKE '%e%';
+-- Write a query to display the last_name, job, and salary of all the employees who don’t work as Programmers or Shipping Clerks, and who don’t receive a salary equal to $4,500, $10,000, or $15,000.
+SELECT last_name, job_id, salary FROM employees
+WHERE job_id NOT IN ((SELECT job_id FROM jobs WHERE job_title='Programmer'),(SELECT job_id FROM jobs WHERE job_title='Shipping Clerk'))
+AND salary NOT IN (4500,15000,10000);
+-- Write a query to display the last names of all employees whose last name contains exactly six characters.
+SELECT last_name FROM employees WHERE LENGTH(last_name)=6;
+-- Write a query to display the last name of all employees who have the letter ‘e’ as the third character in the name.
+SELECT last_name FROM employees WHERE last_name ILIKE '__e%';
+-- Write a query to display the jobs title appearing in the employees table.
+SELECT job_title FROM employees JOIN jobs ON employees.job_id=jobs.job_id GROUP BY job_title;
+-- Write a query to select all information of employees whose last name is either ‘JONES’ or ‘BLAKE’ or ‘SCOTT’ or ‘KING’ or ‘FORD’.
+SELECT * FROM employees WHERE last_name IN  ('Jones','Blake' , 'Scott' , 'King' , 'Ford');
