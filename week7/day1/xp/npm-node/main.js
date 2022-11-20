ded14ab9c9ffc858abc74247b68f16256c1e52ab
NaamaCahen <cahenbb@gmail.com>
2022-11-20 21:02:41 +0200

    xp

---
+++ week7/day1/xp/npm-node/main.js
@@ -0,0 +1,14 @@
+//ex1
+const a = 5;
+const b = 10;
+console.log(`Hello you are ${a+b}`);
+
+//ex2
+console.log("hi how are you?");
+
+//ex3
+// const a = 5;
+// const b = 10;
+const x=a+b;
+const c = 6;
+console.log(x+c);
